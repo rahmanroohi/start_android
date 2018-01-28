c9b94e069484bd91327e10b72c33869b3b62538e
ramezani <ramezani@golden-co.ir>
2018-01-28 13:26:28 +0330

    Initial commit

---
+++ app/src/main/java/jvd/goldenco/start_android/plash.java
@@ -0,0 +1,21 @@
+package jvd.goldenco.start_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class plash extends AppCompatActivity {
+
+    int a=0;
+    int b=12;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plash);
+        a=20;
+        b=30;
+
+
+    }
+}
